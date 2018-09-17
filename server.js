03562fa7d9411c6dea5f59f08d36b8c7f1ccda26
Watit Thammarat <tongnakub@Watits-MacBook-Air.local>
2018-09-17 19:02:51 +0700

    finished graphQL server

---
+++ server.js
@@ -0,0 +1,29 @@
+const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+
+require('dotenv').config({ path: 'variables.env' });
+
+const User = require('./models/User');
+const Post = require('./models/Post');
+const resolvers = require('./resolvers');
+
+const filePath = path.join(__dirname, 'typeDefs.gql');
+const typeDefs = fs.readFileSync(filePath, 'utf-8');
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('DB connected'))
+  .catch(err => console.error(err));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    User,
+    Post
+  }
+});
+
+server.listen().then(({ url }) => console.log(`Server listening on ${url}`));
